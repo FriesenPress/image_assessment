f53a62dafc7838e7193ecda54aedba954101ea7b
David Laberge <dlaberge@friesenpress.com>
2015-01-13 17:05:10 -0800

    Revise layout.

---
+++ image.js
@@ -0,0 +1,90 @@
+// Image functions
+
+Image.prototype.printWidth = function(ppi) {
+    return this.width / ppi;
+};
+
+Image.prototype.printHeight = function(ppi) {
+    return this.height / ppi;
+};
+
+Image.prototype.requiredResizeToMatchArea = function(ppi, targetWidth, targetHeight) {
+    // Returns the percentage the image must be resized to fully cover the target area at the given ppi.
+    return Math.max((targetWidth * ppi - this.width) / this.width, (targetHeight * ppi - this.height) / this.height);
+};
+
+Image.prototype.aspectRatioString = function() {
+	var aspectRatio;
+	var heightRelativeToWidth = (this.height / this.width).toFixed(1);
+	if (heightRelativeToWidth < 1) {
+		var widthRelativeToHeight = (this.width / this.height).toFixed(1);
+		aspectRatio = widthRelativeToHeight + " : 1";
+	} else {
+		aspectRatio= "1 : " + heightRelativeToWidth;
+	}
+	return aspectRatio;
+};
+
+Image.prototype.sizeRating = function(ppi, targetWidth, targetHeight) {
+    var printWidth = this.printWidth(ppi);
+    var printHeight = this.printHeight(ppi);
+    var rating;
+    if (printWidth >= targetWidth && printHeight >= targetHeight ) {
+        rating = 'XL';
+    } else if (printWidth >= targetWidth*.75 && printHeight >= targetHeight*.75 ) {
+        rating = 'L';
+    } else if (printWidth >= targetWidth*.5 && printHeight >= targetHeight*.5 ) {
+        rating = 'M';
+    } else if (printWidth >= targetWidth*.25 && printHeight >= targetHeight*.25 ) {
+        rating = 'S';
+    } else {
+        rating = 'XS';
+    }
+    return rating;
+};
+
+
+Image.prototype.printSizeComment = function(ppi, enlargementTolerancePercent) {
+    var printWidth = ( this.printWidth( ppi) ).toFixed(1);
+    var printHeight = ( this.printHeight( ppi) ).toFixed(1);
+	return "Assuming no cropping, this image can be printed at up to " +
+						printWidth + "\" by " + printHeight + "\" without losing any resolution, " +
+						"or at up to about " + (printWidth * enlargementTolerancePercent) + "\" by " + (printHeight * enlargementTolerancePercent) +
+						"\" with some minor loss of quality.\n";
+};
+
+
+Image.prototype.assessmentComment = function(requiredResize) {
+    var assessmentComment;
+    if (requiredResize <= 0) {
+        assessmentComment = "The resolution of this image should be sufficient.";
+    } else if (requiredResize <= 0.25) {
+        assessmentComment = "Enlargement may be required, but the loss of resolution may not be significant.";
+    } else if (requiredResize <= 0.5) {
+        assessmentComment = "Enlargement would likely be required which would cause significant loss of quality.  It's recommended to use an image with better resolution.";
+    } else {
+        assessmentComment = "This is likely too small.  Please use an image with better resolution."
+    }
+    return assessmentComment;
+};
+
+Image.prototype.aspectRatioWarningThresholdExceeded = function(targetWidth, targetHeight, warningThreshold) {
+    var actualWidthAsPercentOfHeight = this.width / this.height;
+    var targetWidthAsPercentOfHeight = targetWidth / targetHeight;
+    return Boolean((actualWidthAsPercentOfHeight * warningThreshold) < targetWidthAsPercentOfHeight || actualWidthAsPercentOfHeight > (targetWidthAsPercentOfHeight * warningThreshold));
+};
+
+Image.prototype.aspectRatioComment = function(targetWidth, targetHeight, warningThreshold) {
+    var actualWidthAsPercentOfHeight = this.width / this.height;
+    var targetWidthAsPercentOfHeight = targetWidth / targetHeight;
+    var comment;
+    // WidthAsPercentOfHeight.  if 1, the image is a square.  If <1, image is portrait (all trim sizes).  If >1, image is landscape orientation.
+    if ( (actualWidthAsPercentOfHeight * warningThreshold) < targetWidthAsPercentOfHeight ) {
+		comment = 'The image is proportionately taller and skinnier than the target print space.';
+	} else if ( actualWidthAsPercentOfHeight > (targetWidthAsPercentOfHeight * warningThreshold) ) {
+		comment = 'The image is proportionately shorter and wider than the target print space.';
+	} else { // shouldn't happen
+	    comment = 'The image aspect ratio is within tolerance.';
+	}
+	return comment;
+};
--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 ////// DEFAULT VARIABLES
+var DEFAULT_PPI = 300;
 
 var DEFAULT_ASPECT_RATIO_WARNING_THRESHOLD = 1.5;
 var DEFAULT_ENLARGEMENT_TOLERANCE_PERCENT = 1.25;
-var DEFAULT_IMAGE_URL = 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/800px-Shakespeare.jpg';
 var DEFAULT_INTERIOR_IMAGE_TOLERANCE_PERCENT = .75;
-var DEFAULT_PPI = 300;
-var DEFAULT_PREVIEW_WIDTH = 200;
+
+var DEFAULT_IMAGE_URL = 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/800px-Shakespeare.jpg';
+var PLACEHOLDER_IMAGE = "ph.png";
+
 var DEFAULT_PREVIEW_HEIGHT = 200;
+var DEFAULT_PREVIEW_WIDTH = 200;
 var DEFAULT_PREVIEW_SETTINGS = {width:DEFAULT_PREVIEW_WIDTH,height:DEFAULT_PREVIEW_HEIGHT,method:'resize',fitDirection:'center center'};
+
 var DEFAULT_TRIM_SIZES = [
 	{'width': 5, 	'height': 5},
 	{'width': 5.5, 	'height': 8.5},
@@ -16,105 +20,6 @@ var DEFAULT_TRIM_SIZES = [
 	{'width': 8.5, 	'height': 8.5},
 	{'width': 8.5, 	'height': 11}
 ];
-var PLACEHOLDER_IMAGE = "ph.png";
-
-Image.prototype.printWidth = function(ppi) {
-    return this.width / ppi;
-};
-
-Image.prototype.printHeight = function(ppi) {
-    return this.height / ppi;
-};
-
-Image.prototype.requiredResizeToMatchArea = function(ppi, targetWidth, targetHeight) {
-    // Returns the percentage the image must be resized to fully cover the target area at the given ppi.
-    return Math.max((targetWidth * ppi - this.width) / this.width, (targetHeight * ppi - this.height) / this.height);
-};
-
-Image.prototype.aspectRatioString = function() {
-	var aspectRatio;
-	var heightRelativeToWidth = (this.height / this.width).toFixed(1);
-	if (heightRelativeToWidth < 1) {
-		var widthRelativeToHeight = (this.width / this.height).toFixed(1);
-		aspectRatio = widthRelativeToHeight + " : 1";
-	} else {
-		aspectRatio= "1 : " + heightRelativeToWidth;
-	}
-	return aspectRatio;
-};
-
-Image.prototype.sizeRating = function(ppi, targetWidth, targetHeight) {
-    var printWidth = this.printWidth(ppi);
-    var printHeight = this.printHeight(ppi);
-    var rating;
-    if (printWidth >= targetWidth && printHeight >= targetHeight ) {
-        rating = 'XL';
-    } else if (printWidth >= targetWidth*.75 && printHeight >= targetHeight*.75 ) {
-        rating = 'L';
-    } else if (printWidth >= targetWidth*.5 && printHeight >= targetHeight*.5 ) {
-        rating = 'M';
-    } else if (printWidth >= targetWidth*.25 && printHeight >= targetHeight*.25 ) {
-        rating = 'S';
-    } else {
-        rating = 'XS';
-    }
-    return rating;
-};
-
-
-var Assessment = function(img) {
-    this.img = img;
-};
-
-Assessment.prototype.assessmentComment = function(imageUsage, requiredResize) {
-    var assessmentComment;
-    if (imageUsage == 'interior') {
-        assessmentComment = "";
-    } else {
-        if (requiredResize <= 0) {
-            assessmentComment = "The resolution of this image should be sufficient.";
-        } else if (requiredResize <= 0.25) {
-            assessmentComment = "Enlargement may be required, but the loss of resolution may not be significant.";
-        } else if (requiredResize <= 0.5) {
-            assessmentComment = "Enlargement would likely be required which would cause significant loss of quality.  It's recommended to use an image with better resolution.";
-        } else {
-            assessmentComment = "This is likely too small.  Please use an image with better resolution."
-        }
-    }
-    return assessmentComment;
-};
-
-Assessment.prototype.resolutionComment = function(ppi, enlargementTolerancePercent) {
-    var printWidth = ( this.img.printWidth(ppi) ).toFixed(1);
-    var printHeight = ( this.img.printHeight(ppi) ).toFixed(1);
-	return "Assuming no cropping, this image can be printed at up to " +
-						printWidth + "\" by " + printHeight + "\" without losing any resolution, " +
-						"or at up to about " + (printWidth * enlargementTolerancePercent) + "\" by " + (printHeight * enlargementTolerancePercent) +
-						"\" with some minor loss of quality.\n";
-};
-
-Assessment.prototype.aspectRatioComment = function(imageUsage, targetWidth, targetHeight, warningThreshold) {
-    var actualWidthAsPercentOfHeight = this.img.width / this.img.height;
-    var targetWidthAsPercentOfHeight; // width divided by height
-    if (imageUsage == 'cover' || imageUsage == 'interior') {
-        targetWidthAsPercentOfHeight = targetWidth / targetHeight;
-    } else if (imageUsage == 'spread') {
-        targetWidthAsPercentOfHeight = ( targetWidth * 2) / targetHeight;
-    } else { // shouldn't happen
-        targetWidthAsPercentOfHeight = 1;
-    }
-    var comment;
-    // WidthAsPercentOfHeight.  if 1, the image is a square.  If <1, image is portrait (all trim sizes).  If >1, image is landscape orientation.
-    if ( imageUsage =='interior') {
-        return comment;
-    } else if ( (actualWidthAsPercentOfHeight * warningThreshold) < targetWidthAsPercentOfHeight ) {
-		comment = 'The image is proportionately taller and skinnier than the target print space.';
-	} else if ( actualWidthAsPercentOfHeight > (targetWidthAsPercentOfHeight * warningThreshold) ) {
-		comment = 'The image is proportionately shorter and wider than the target print space.';
-	}
-	return comment;
-};
-
 
 
 function initializeForm() {
@@ -125,6 +30,7 @@ function initializeForm() {
 	$( "#input-url" ).hide();
 
     $( ".result" ).hide();
+
 
     $( ".image-attribute").hide();
 	$( ".preview-container" ).nailthumb(DEFAULT_PREVIEW_SETTINGS);
@@ -144,98 +50,6 @@ function initializeForm() {
 		}));
 	}
 } // end of function initializeForm
-
-
-function getTargetDimension(dimension, trimSize, imageUsage, interiorImageTolerance) {
-    var spreadFactor;
-    if (dimension == 'width') {
-        spreadFactor = 2;
-    } else {
-        spreadFactor = 1;
-    }
-
-    var targetDimension;
-    if (imageUsage == 'interior') {
-        targetDimension = trimSize[dimension] * interiorImageTolerance;
-    } else if (imageUsage == 'spread') {
-        targetDimension = trimSize[dimension] * spreadFactor;
-    } else { // 'cover', or any other case
-        targetDimension = trimSize[dimension];
-    }
-    return targetDimension;
-}
-
-
-function previewImageFromURL(src) {
-    var previewImg = new Image;
-    previewImg.onload = function() {
-        $(".preview-container").nailthumb(DEFAULT_PREVIEW_SETTINGS);
-        $("#input-preview").attr('src', src);
-        $(".aspect-ratio").text(previewImg.aspectRatioString());
-        $(".width-value-px").text(previewImg.width);
-        $(".height-value-px").text(previewImg.height);
-        $(".image-attribute").show(); // width in px, height in px, aspectRatio str
-    };
-    previewImg.src = src;
-}
-
-
-function displayAssessment(img, ppi, targetWidth, targetHeight, imageUsage, aspectRatioWarningThreshold, enlargementTolerancePercent) {
-    var requiredResize = img.requiredResizeToMatchArea(ppi, targetWidth, targetHeight);
-    var aspectRatioString = img.aspectRatioString();
-
-    var assessment = new Assessment(img);
-    var aspectRatioComment = assessment.aspectRatioComment(imageUsage, targetWidth, targetHeight, aspectRatioWarningThreshold);
-    var resolutionComment = assessment.resolutionComment(ppi, enlargementTolerancePercent);
-    var assessmentComment = assessment.assessmentComment(imageUsage, requiredResize);
-
-    var colourStyle, progressBarValue, resolutionIcon;
-    if (requiredResize < 0) {
-        colourStyle = "success";
-        progressBarValue = 100;
-        resolutionIcon = "<i class=\"fa fa-thumbs-up\"></i>";
-    } else if (requiredResize < 0.25) {
-        colourStyle = "info";
-        progressBarValue = 75;
-        resolutionIcon = "<i class=\"fa fa-thumbs-up\"></i>";
-    } else if (requiredResize < 0.5) {
-        colourStyle = "warning";
-        progressBarValue = 50;
-        resolutionIcon = "<i class=\"fa fa-thumbs-down\"></i>";
-    } else {
-        colourStyle = "danger";
-        progressBarValue = 25;
-        resolutionIcon = "<i class=\"fa fa-thumbs-down\"></i>";
-    }
-
-    var printWidth = ( img.printWidth(ppi) ).toFixed(1);
-    var printHeight = ( img.printHeight(ppi) ).toFixed(1);
-
-    $( ".print-width" ).text( printWidth );
-	$( ".print-height" ).text( printHeight );
-	$( ".aspect-ratio" ).text( aspectRatioString );
-	$( ".print-measurements" ).show();
-
-	$( ".progress-bar" ).attr(
-		{ 	'class': "progress-bar progress-bar-"+colourStyle,
-			'aria-valuenow': progressBarValue,
-			'style': "width:"+progressBarValue+"%" }
-	);
-
-	if (typeof aspectRatioComment !== 'undefined') {
-        $( "#aspect-ratio-comment" ).text( aspectRatioComment ).show();
-    }
-
-    if (imageUsage == 'interior') {
-        var sizeRating = img.sizeRating(ppi, targetWidth, targetHeight);
-        $( ".assessment-comment" ).html("Image size: " + "<span class=\"size-rating-container\"><strong class=\"size-rating\">" + sizeRating + "</strong></span>").show();
-        $( ".resolution-comment" ).html( resolutionComment ).show();
-    } else {
-        $( ".assessment-comment" ).html( assessmentComment ).show();
-        $( ".resolution-comment" ).html( resolutionIcon + resolutionComment ).show();
-    }
-
-} // end function displayAssessment
 
 
 // Get the input values
@@ -258,6 +72,114 @@ function getUrlInput() {
 }
 
 
+// Calculation functions
+
+function getTargetDimension(dimension, trimSize, imageUsage, interiorImageTolerance) {
+    var spreadFactor = ((dimension == 'width') ? 2 : 1);
+    var targetDimension;
+    if (imageUsage == 'interior') {
+        targetDimension = trimSize[dimension] * interiorImageTolerance;
+    } else if (imageUsage == 'spread') {
+        targetDimension = trimSize[dimension] * spreadFactor;
+    } else { // 'cover', or any other case
+        targetDimension = trimSize[dimension];
+    }
+    return targetDimension;
+}
+
+function getAssessmentIcon(requiredResize) {
+    var assessmentIcon;
+    if (requiredResize < 0.25) {
+        assessmentIcon = "<i class=\"fa fa-thumbs-up\"></i>";
+    } else {
+        assessmentIcon = "<i class=\"fa fa-thumbs-down\"></i>";
+    }
+    return assessmentIcon;
+}
+
+function getProgressBarValue(requiredResize) {
+    var progressBarValue;
+    if (requiredResize < 0) {
+        progressBarValue = 100;
+    } else if (requiredResize < 0.25) {
+        progressBarValue = 75;
+    } else if (requiredResize < 0.5) {
+        progressBarValue = 50;
+    } else {
+        progressBarValue = 25;
+    }
+    return progressBarValue;
+}
+
+function getProgressBarColour(requiredResize) {
+    var progressBarColour;
+    if (requiredResize < 0) {
+        progressBarColour = "success";
+    } else if (requiredResize < 0.25) {
+        progressBarColour = "info";
+    } else if (requiredResize < 0.5) {
+        progressBarColour = "warning";
+    } else {
+        progressBarColour = "danger";
+    }
+    return progressBarColour;
+}
+
+
+// Display functions
+
+function previewImageFromURL(src) {
+    var previewImg = new Image;
+    previewImg.onload = function() {
+        $(".preview-container").nailthumb(DEFAULT_PREVIEW_SETTINGS);
+        $("#input-preview").attr('src', src);
+        $(".aspect-ratio").text(previewImg.aspectRatioString());
+        $(".width-value-px").text(previewImg.width);
+        $(".height-value-px").text(previewImg.height);
+        $(".image-attribute").show(); // width in px, height in px, aspectRatio str
+    };
+    previewImg.src = src;
+}
+
+function displayAssessment(img, ppi, targetWidth, targetHeight, imageUsage, aspectRatioWarningThreshold, enlargementTolerancePercent) {
+    if (img.aspectRatioWarningThresholdExceeded()) {
+        var aspectRatioComment = img.aspectRatioComment(targetWidth, targetHeight, aspectRatioWarningThreshold);
+        $( "#aspect-ratio-comment" ).text( aspectRatioComment ).show();
+    }
+
+    var printWidth = ( img.printWidth(ppi) ).toFixed(1);
+    var printHeight = ( img.printHeight(ppi) ).toFixed(1);
+    $( ".print-width" ).text( printWidth );
+	$( ".print-height" ).text( printHeight );
+	$( ".print-measurements" ).show();
+
+    var printSizeComment = img.printSizeComment(ppi, enlargementTolerancePercent);
+    $( ".print-size-comment" ).html( printSizeComment ).show();
+
+
+    var requiredResize = img.requiredResizeToMatchArea(ppi, targetWidth, targetHeight);
+
+    var sizeRating = img.sizeRating(ppi, targetWidth, targetHeight);
+    $( ".size-rating").text( sizeRating );
+    $( ".size-rating-container").show();
+
+    var progressBarValue = getProgressBarValue(requiredResize);
+    var progressBarColour = getProgressBarColour(requiredResize);
+    $( ".progress").show();
+    $( ".progress-bar" ).attr(
+        { 	'class': "progress-bar progress-bar-"+progressBarColour,
+            'aria-valuenow': progressBarValue,
+            'style': "width:"+progressBarValue+"%" }
+    ).show();
+
+    var assessmentComment = img.assessmentComment(requiredResize);
+    var assessmentIcon = getAssessmentIcon(requiredResize);
+    $( ".assessment-comment" ).html( assessmentIcon + ' ' + assessmentComment ).show();
+
+} // end function displayAssessment
+
+
+
 $(document).ready(function() {
 
 	initializeForm();
@@ -265,8 +187,7 @@ $(document).ready(function() {
 	$( "#image-from-url" ).change(function() {
 		if (this.checked) {
 			$( "#input-file" ).hide();
-			$( "#input-url").val(DEFAULT_IMAGE_URL);
-			$( "#input-url" ).show();
+			$( "#input-url").val(DEFAULT_IMAGE_URL).show();
 	        $( "#input-preview" ).attr('src', PLACEHOLDER_IMAGE);
             $( ".image-attribute").hide();
 		}
@@ -284,7 +205,8 @@ $(document).ready(function() {
 	$( ".form-group" ).change(function() {
             $( "#invalid-source-flash").hide();
 			$( ".result" ).hide();
-			$( ".progress-bar" ).attr({	'class': "progress-bar", 'aria-valuenow': 0, style: "width:0%" });
+            $( ".progress").hide();
+			//$( ".progress-bar" ).attr({	'class': "progress-bar", 'aria-valuenow': 0, style: "width:0%" });
 	});
 
 	$( "#input-file" ).change(function() {
